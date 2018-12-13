fe1c05c65213f421c3d281f31b78b4379ca3ec41
Markus Winter <markus.winter@students.fhnw.ch>
2018-12-13 22:32:19 +0100

    controller & service

---
+++ grails-app/controllers/webec/ExamController.groovy
@@ -0,0 +1,101 @@
+package webec
+
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
+class ExamController {
+
+    ExamService examService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    /*def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond examService.list(params), model:[examCount: examService.count()]
+    }*/
+
+    def scaffold = Exam
+
+    def show(Long id) {
+        respond examService.get(id)
+    }
+
+    def create() {
+        respond new Exam(params)
+    }
+
+    def save(Exam exam) {
+        if (exam == null) {
+            notFound()
+            return
+        }
+
+        try {
+            examService.save(exam)
+        } catch (ValidationException e) {
+            respond exam.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'exam.label', default: 'Exam'), exam.id])
+                redirect exam
+            }
+            '*' { respond exam, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond examService.get(id)
+    }
+
+    def update(Exam exam) {
+        if (exam == null) {
+            notFound()
+            return
+        }
+
+        try {
+            examService.save(exam)
+        } catch (ValidationException e) {
+            respond exam.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'exam.label', default: 'Exam'), exam.id])
+                redirect exam
+            }
+            '*'{ respond exam, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        examService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'exam.label', default: 'Exam'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'exam.label', default: 'Exam'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/controllers/webec/LecturerController.groovy
@@ -0,0 +1,99 @@
+package webec
+
+import grails.validation.ValidationException
+import static org.springframework.http.HttpStatus.*
+
+class LecturerController {
+
+    LecturerService lecturerService
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond lecturerService.list(params), model:[lecturerCount: lecturerService.count()]
+    }
+
+    def show(Long id) {
+        respond lecturerService.get(id)
+    }
+
+    def create() {
+        respond new Lecturer(params)
+    }
+
+    def save(Lecturer lecturer) {
+        if (lecturer == null) {
+            notFound()
+            return
+        }
+
+        try {
+            lecturerService.save(lecturer)
+        } catch (ValidationException e) {
+            respond lecturer.errors, view:'create'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'lecturer.label', default: 'Lecturer'), lecturer.id])
+                redirect lecturer
+            }
+            '*' { respond lecturer, [status: CREATED] }
+        }
+    }
+
+    def edit(Long id) {
+        respond lecturerService.get(id)
+    }
+
+    def update(Lecturer lecturer) {
+        if (lecturer == null) {
+            notFound()
+            return
+        }
+
+        try {
+            lecturerService.save(lecturer)
+        } catch (ValidationException e) {
+            respond lecturer.errors, view:'edit'
+            return
+        }
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'lecturer.label', default: 'Lecturer'), lecturer.id])
+                redirect lecturer
+            }
+            '*'{ respond lecturer, [status: OK] }
+        }
+    }
+
+    def delete(Long id) {
+        if (id == null) {
+            notFound()
+            return
+        }
+
+        lecturerService.delete(id)
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'lecturer.label', default: 'Lecturer'), id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'lecturer.label', default: 'Lecturer'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}

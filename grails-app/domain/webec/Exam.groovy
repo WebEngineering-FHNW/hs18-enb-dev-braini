feb285b1b7b3737e2f870c33488406a67dc375b9
Markus Winter <markus.winter@students.fhnw.ch>
2018-12-15 01:54:53 +0100

    add description to Exam domain

--- grails-app/domain/webec/Exam.groovy
+++ grails-app/domain/webec/Exam.groovy
@@ -2,6 +2,7 @@ package webec
 
 class Exam {
     Date       date
+    String     description
     Integer    weighting = 100
     BigDecimal grade
 
@@ -9,9 +10,10 @@ class Exam {
     static belongsTo = [module:Module]
 
     static constraints = {
-        date      (nullable:false)
-        weighting (nullable:false, min: 1, max: 100)
-        grade     (nullable:true, min: 1.0, max: 6.0)
+        date        (nullable:false)
+        description (nullable:true, size: 0..50)
+        weighting   (nullable:false, min: 1, max: 100)
+        grade       (nullable:true, min: 1.0, max: 6.0)
     }
 
     String toString(){
